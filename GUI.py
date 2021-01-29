b05846deac1f21cef282ea3cee8b8afe8c36fe86
Zhang Hua <zhanghua.00@qq.com>
2021-01-29 21:02:53 +0800

    修复滑块无法拖动的问题

--- GUI.py
+++ GUI.py
@@ -56,26 +56,26 @@ class CustomListWidget(QListWidget):
             self.show_buttons()
 class FixedSlider(QSlider):
     def __init__(self):
-        super(QSlider,self).__init__()
+        super(FixedSlider,self).__init__()
         self.setTickInterval(1)
         self.value_label=QLabel(self)    
         self.value_label.setFixedSize(40,20) 
         self.value_label.setAutoFillBackground(True)
-        self.value_label.setStyleSheet("QLabel{background:transparent;}")
+        self.value_label.setStyleSheet("QLabel{background:transparent;font:8px}")
         self.value_label.setAlignment(Qt.Alignment.AlignCenter)
         self.value_label.setVisible(False)
         self.value_label.move(0,-5)
     def mousePressEvent(self,event:QMouseEvent):
-        super(QSlider,self).mousePressEvent(event)
+        super(FixedSlider,self).mousePressEvent(event)
         if self.value_label.isVisible()==False:
             self.value_label.setVisible(True)
             self.value_label.setText(str(self.value()/10))
     def mouseMoveEvent(self,event:QMouseEvent):
-        super(QSlider,self).mouseMoveEvent(event)
+        super(FixedSlider,self).mouseMoveEvent(event)
         self.value_label.setText(str(self.value()/10))
         self.value_label.move(int((self.width()-self.value_label.width())*self.value()/(self.maximum()-self.minimum())),-5)
     def mouseReleaseEvent(self,event:QMouseEvent):
-        super(QSlider,self).mouseReleaseEvent(event)
+        super(FixedSlider,self).mouseReleaseEvent(event)
         if self.value_label.isVisible()==True:
             self.value_label.setVisible(False)
         
